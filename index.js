7760491b0da5b29dc53d373683b7d8fa4939802b
野良猫しぃ <neko@noranekofactory.red>
2024-03-21 07:35:53 +0900

    first

---
+++ index.js
@@ -0,0 +1,15 @@
+const path = require("path");
+const fs = require("fs");
+
+const packageStr = fs.readFileSync(path.resolve(__dirname, "package.json"), {
+  encoding: "utf-8",
+});
+const package = JSON.parse(packageStr);
+
+const nameOption = process.argv.includes("--name");
+
+if (nameOption) {
+  console.log(package.name);
+} else {
+  console.log("オプションがありません");
+}
